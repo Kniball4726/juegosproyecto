f78ecf4c1740784be19c727aa9d6bbb3df5d0fe5
Gregory Rodriguez <glrd4726ml@gmail.com>
2026-05-02 19:07:42 -0300

    Pruebas

---
+++ app.py
@@ -0,0 +1,4 @@
+import src.menu as menu
+if __name__ == "__main__":
+    menu.menu()
+    
--- src/menu.py
+++ src/menu.py
@@ -1,24 +1,28 @@
+import os
+
 usuario:str="Gregory"
 contra:str="1234"
 user:str=""
-contra:str=""
+password:str=""
+contador:int=0
+
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def iniciar_sesion():
-    user = input("Ingrese su nombre de usuario: ")
-    password = input("Ingrese su contraseña: ")
-    if user == usuario and password == contra:
-        print("Inicio de sesión exitoso.")
-    else:
-        print("Nombre de usuario o contraseña incorrectos.")
-        menu()
+    print("Iniciar sesión\n")
+    
+                
 
 
 def menu():
-    print("Bienvenido al sistema de inicio de sesión")
-    print("1. Iniciar sesión")
-    print("2. Salir")
+    limpiar_pantalla()
+    print("Bienvenido al sistema de Juegos desaarrollados en Python\n")
+    print("1. Iniciar sesión\n2.- Salir")
     opcion = input("Seleccione una opción: ")
     if opcion == "1":
+        limpiar_pantalla()
         iniciar_sesion()
     elif opcion == "2":
         print("Saliendo del sistema...")
@@ -26,3 +30,4 @@ def menu():
         print("Opción no válida. Intente nuevamente.")
         menu()
 
+menu()
